e82840a126b9b106339e2991fa192207126c0544
Yasmin Rodriguez <yasmin.rodriguez@gmail.com>
2017-10-05 08:54:10 -0400

    Separate method for collecting input, it keeps prompting until valid input. Simplified the calculation for Sum of Digits.

--- src/preWorkMathChallenge/MathChallenge.java
+++ src/preWorkMathChallenge/MathChallenge.java
@@ -3,10 +3,8 @@ package preWorkMathChallenge;
 import java.util.Scanner;
 
 /**
- * This program reads two integers from the user. It validates that both
- * integers have the same number of digits and have at least two digits. If the
- * two integers are valid, it returns whether the sum of the digits in the same
- * position on each integer add up to the same total.
+ * This program reads two integers from the user and returns whether the sum of
+ * the digits on each position on both integers adds up to the same total.
  * 
  * @author Yasmin
  *
@@ -14,42 +12,26 @@ import java.util.Scanner;
 public class MathChallenge {
 
 	/**
-	 * Prompts the user for two numbers. Validates that input is numeric, then calls
-	 * methods to validate that length of both numbers and to check if the sum of
-	 * the digits is equal.
+	 * Calls methods to collect input, validate it, and compute the sum of the
+	 * digits.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 		int number1 = 0;
 		int number2 = 0;
-		final String NOT_A_NUMBER_ERROR = "You must enter a number. Please try again.";
 		MathChallenge m = new MathChallenge();
 		boolean validIntegers = false;
 		boolean sumOfDigitsIsEqual = false;
 
-		System.out.println("Please enter the first number: ");
-		if (input.hasNextInt()) {
-			number1 = input.nextInt();
-		} else {
-			System.out.println(NOT_A_NUMBER_ERROR);
-			return;
-		}
-
-		System.out.println("Please enter the second number: ");
-		if (input.hasNextInt()) {
-			number2 = input.nextInt();
-		} else {
-			System.out.println(NOT_A_NUMBER_ERROR);
-			return;
-		}
+		number1 = getNumber();
+		number2 = getNumber();
 
 		validIntegers = m.validateLength(number1, number2);
 
 		if (validIntegers) {
 			sumOfDigitsIsEqual = m.sumOfDigitsIsEqual(number1, number2);
-			System.out.println("Sum of Digits is Equal: " + sumOfDigitsIsEqual);
+			System.out.println("Sum of digits in each position is equal: " + sumOfDigitsIsEqual);
 		} else {
 			System.out.println(
 					"Both numbers must have at least two digits and be the same length. Please try again.");
@@ -57,45 +39,64 @@ public class MathChallenge {
 	}
 
 	/**
+	 * Get a number from the user. If input is not numeric, it keeps prompting until
+	 * they enter a valid number.
+	 * 
+	 * @return the number input by the user
+	 */
+	private static int getNumber() {
+		Scanner input = new Scanner(System.in);
+		int number = 0;
+		boolean numericInput = false;
+
+		do {
+			System.out.println("Please enter a number: ");
+
+			if (numericInput = input.hasNextInt()) {
+				number = input.nextInt();
+			}
+			else {
+				input.next();
+			}
+		} while (!numericInput);
+
+		return number;
+	}
+
+	/**
 	 * Validates that both inputs are the same length and at least two digits long.
 	 * 
-	 * @param input1
+	 * @param number1
 	 *            the first number
-	 * @param input2
+	 * @param number2
 	 *            the second number
 	 * @return true if both numbers are the same length
 	 */
 	public boolean validateLength(int number1, int number2) {
 		int lengthNumber1 = Integer.toString(number1).length();
 		int lengthNumber2 = Integer.toString(number2).length();
-		return (lengthNumber1 == lengthNumber2) && (lengthNumber1 > 1) & (lengthNumber2 > 1);
+		return (lengthNumber1 == lengthNumber2) && (lengthNumber1 > 1) && (lengthNumber2 > 1);
 	}
 
 	/**
-	 * Validates that the sum of the digits in each position in both numbers is
+	 * Checks whether the sum of the digits on each position on both integers is
 	 * equal
 	 * 
-	 * @param number1
+	 * @param num1
 	 *            the first number
-	 * @param number2
+	 * @param num2
 	 *            the second number
-	 * @return true if the sum of the digits in each place is the same
+	 * @return true if the sum of the digits on each position is the same
 	 */
-	public boolean sumOfDigitsIsEqual(int number1, int number2) {
-		int numDigits = 0;
-		int previousSum = 0;
+	public boolean sumOfDigitsIsEqual(int num1, int num2) {
+		String number1 = Integer.toString(num1);
+		String number2 = Integer.toString(num2);
+		int sum = number1.charAt(0) + number2.charAt(0);
 
-		while (number1 > 0 && number2 > 0) {
-			int sum = (number1 % 10) + (number2 % 10);
-			if (numDigits > 0 && sum != previousSum) {
+		for (int i = 1; i < number1.length(); i++) {
+			if ((number1.charAt(i) + number2.charAt(i)) != sum) {
 				return false;
 			}
-
-			previousSum = sum;
-			++numDigits;
-
-			number1 /= 10;
-			number2 /= 10;
 		}
 		return true;
 	}
